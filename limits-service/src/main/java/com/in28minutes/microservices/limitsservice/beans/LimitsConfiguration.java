4c6b52c6bdebcf9ac06a611da493db624eca2c6d
LuisPereir4 <49247322+LuisPereir4@users.noreply.github.com>
2019-11-12 20:20:14 -0300

    Limits-service uploaded to git

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/beans/LimitsConfiguration.java
@@ -0,0 +1,29 @@
+package com.in28minutes.microservices.limitsservice.beans;
+
+public class LimitsConfiguration {
+    private int max;
+    private int min;
+
+    public LimitsConfiguration() {}
+
+    public LimitsConfiguration(int max, int min) {
+        this.max = max;
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+}
