d7366703f691a0fbd9ee2e05f44c2f1814d8651e
LuisPereir4 <49247322+LuisPereir4@users.noreply.github.com>
2019-11-12 20:24:14 -0300

    Lombok added in the project

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/beans/LimitsConfiguration.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/beans/LimitsConfiguration.java
@@ -1,29 +1,18 @@
 package com.in28minutes.microservices.limitsservice.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
 public class LimitsConfiguration {
+    @Getter
+    @Setter
     private int max;
+    @Getter
+    @Setter
     private int min;
 
     public LimitsConfiguration() {}
-
-    public LimitsConfiguration(int max, int min) {
-        this.max = max;
-        this.min = min;
-    }
-
-    public int getMax() {
-        return max;
-    }
-
-    public void setMax(int max) {
-        this.max = max;
-    }
-
-    public int getMin() {
-        return min;
-    }
-
-    public void setMin(int min) {
-        this.min = min;
-    }
 }
